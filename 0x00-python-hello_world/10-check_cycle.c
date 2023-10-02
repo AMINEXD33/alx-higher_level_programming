ea3a77f8c996ae0af46c7be68b755048b4990ef0
Amine Meftah <amine.gamer222@gmail.com>
2023-10-03 00:08:31 +0100

    fiding a cycle but in O(1) space

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stddef.h>
+/**
+ *check_cycle- this function uses the technique of slow and
+ *fast pointer to check, if a linked list has a cycle
+ *@list: the head of the linked list
+ *Return: 1 if a cycle is found , 0 otherwise
+ */
+int check_cycle(listint_t *list)
+{
+	struct listint_s *fast_p = list;
+	struct listint_s *slow_p = list;
+	int flag;
+
+	flag = 0;
+	while (1)
+	{
+		/*point to the next node*/
+		fast_p = fast_p->next;
+
+		/*this code is eq to waiting a turn then doing something*/
+		if (flag == 0)
+			flag = 1;
+		else if  (flag == 1)
+		{
+			flag = 0;
+			slow_p = slow_p->next;
+		}
+		/*if the fast pointer hit the NULL 0, if fast==slow 1*/
+		if (fast_p == NULL)
+			return (0);
+		else if (fast_p == slow_p)
+			return (1);
+	}
+}
+
