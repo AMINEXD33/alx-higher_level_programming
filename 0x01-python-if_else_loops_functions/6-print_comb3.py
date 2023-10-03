07de605815088ee7bc0c54d202691c7bfedea043
Amine Meftah <amine.gamer222@gmail.com>
2023-10-03 19:10:17 +0100

    py stuff

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i+1, 10):
-        print("{:02d}, ".format(i*10 + j), end="")
-
-print("{:02d}".format(98))
+        if ((i * 10 +j) == 89):
+            print("{:02d}".format(89), end="\n")
+            break;
+        print("{:02d}, ".format(i * 10 + j), end="")
