b9c480d06014a533b79b6bd4c601dd6a54cc524a
Amine Meftah <amine.gamer222@gmail.com>
2023-10-03 21:53:11 +0100

    py remove at index n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-	cpy = ""
-	for x in range(len(str)):
-		if (x != n):
-			cpy += str[x]
-			
-	return(cpy)
+    cpy = ""
+    for x in range(len(str)):
+        if (x != n):
+            cpy += str[x]
+    return cpy
