965dd4d46aed242a134dc8eef6bd656a3db1cf23
Amine Meftah <amine.gamer222@gmail.com>
2023-10-03 19:34:14 +0100

    py stuff

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def islower(c):
-    if (c>="a" and c<="z"):
+    if (c >= "a" and c <= "z"):
         return True
     elif (c == ''):
         raise Exception
-    else: return False
+    else: 
+        return False
