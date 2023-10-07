a50dd66dff0f7d22dc8056f3932a330a7cff1519
Amine Meftah <amine.gamer222@gmail.com>
2023-10-07 19:59:41 +0100

    py stuf

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,7 +4,7 @@ def new_in_list(my_list, idx, element):
     for x in my_list:
         cp_list.append(x)
 
-    if (idx < 0 or idx > len(my_list)-1):
+    if (idx < 0 or idx > len(my_list) - 1):
         return cp_list
     
     cp_list[idx] = element
