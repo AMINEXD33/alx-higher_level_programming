402d40f7f610a4689a79e9d7b1e6fd762472b9f2
Amine Meftah <amine.gamer222@gmail.com>
2023-10-07 19:51:54 +0100

    py stuf

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     if(my_list == None):
         return
-    else if(len(my_list) == 0):
+    if(len(my_list) == 0):
         return
     len_ = len(my_list) * -1
     x = -1
