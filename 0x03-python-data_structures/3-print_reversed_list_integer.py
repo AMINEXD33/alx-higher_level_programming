bb2fd0ad81913d4645395fde5944197535c0cb25
Amine Meftah <amine.gamer222@gmail.com>
2023-10-07 19:58:49 +0100

    py stuf

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if (my_list == None) or (len(my_list) == 0)):
-        return
-    len_ = len(my_list) * -1
-    x = -1
-    while (x >= len_):
-        print("{:d}".format(my_list[x]))
-        x -= 1
+    if not my_list:
+        pass
+    else:
+        my_list.reverse()
+        for i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
