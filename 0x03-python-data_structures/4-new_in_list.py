7feca8dd0e14895c4e786a375599a5bfffa44676
Amine Meftah <amine.gamer222@gmail.com>
2023-10-07 20:10:08 +0100

    py stuf

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,8 +3,8 @@
 def new_in_list(my_list, idx, element):
     cp_list = my_list[:]
 
-    if (idx < 0) or (idx > len(my_list) - 1):
-        return cp_list
+    if idx < 0 or idx > len(my_list) - 1:
+        return (cp_list)
     
     cp_list[idx] = element
     return (cp_list)
