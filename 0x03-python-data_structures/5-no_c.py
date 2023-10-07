4173345758504304b9f25de72a185e9c96e9737d
Amine Meftah <amine.gamer222@gmail.com>
2023-10-07 20:18:27 +0100

    py stuf

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def no_c (my_string):
+def no_c(my_string):
     new_str = ""
 
     for x in range(len(my_string)):
-        if my_string[x] != 'c' and  my_string[x] != 'C':
+        if my_string[x] != 'c' and my_string[x] != 'C':
             new_str += my_string[x]
-    return (new_str)
+    return new_str
