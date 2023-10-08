0c5c3e1191ae7e6699cc58d55da435644450190d
Amine Meftah <amine.gamer222@gmail.com>
2023-10-08 14:20:05 +0100

    palindrome check

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdio.h>
+
+int is_palindrome(listint_t **head)
+{
+	struct listint_s *node = *head; 
+	long long int forward;
+	long long int reverce;
+
+	forward = 1;
+	reverce = 1;
+	while (node != NULL)
+	{
+		forward = ((forward) + (node->n)) % sizeof(long long int) + 1;
+		reverce = ((reverce) + (node->n * -1)) % sizeof(long long int) + 1;
+
+		node = node->next;
+
+
+	}
+	if (forward != reverce)
+		return (0)
+
+	return (1);
+}
