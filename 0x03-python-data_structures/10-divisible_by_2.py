245a6ad2080a265ec68da4cbab60a6391ab67cbe
Amine Meftah <amine.gamer222@gmail.com>
2023-10-08 12:17:46 +0100

    py stuf

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
     result = []
+    if my_list is None or len(my_list) == 0:
+        return None
     for x in my_list:
         if (x % 2) == 0:
             result.append(True)
         else:
             result.append(False)
+    return result
