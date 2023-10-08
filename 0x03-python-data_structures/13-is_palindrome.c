cde99296594ecf7a6d27e7f40f5eaa8897b0a4f4
Amine Meftah <amine.gamer222@gmail.com>
2023-10-08 22:24:24 +0100

    palindrome check

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,29 +1,114 @@
 #include "lists.h"
-#include <stdio.h>
+
 /**
- *is_palindrome-this function checks if
- *a a linkd list is palindrome.
- *@head: the head of the linked list
- *Return: 0 if it's not a palindrome, 1 if it is
+ * reverse - reverses the second half of the list
+ *
+ * @h_r: head of the second half
+ * Return: no return
+ */
+void reverse(listint_t **h_r)
+{
+	listint_t *prv;
+	listint_t *crr;
+	listint_t *nxt;
+
+	prv = NULL;
+	crr = *h_r;
+
+	while (crr != NULL)
+	{
+		nxt = crr->next;
+		crr->next = prv;
+		prv = crr;
+		crr = nxt;
+	}
+
+	*h_r = prv;
+}
+
+/**
+ * compare - compares each int of the list
+ *
+ * @h1: head of the first half
+ * @h2: head of the second half
+ * Return: 1 if are equals, 0 if not
+ */
+int compare(listint_t *h1, listint_t *h2)
+{
+	listint_t *tmp1;
+	listint_t *tmp2;
+
+	tmp1 = h1;
+	tmp2 = h2;
+
+	while (tmp1 != NULL && tmp2 != NULL)
+	{
+		if (tmp1->n == tmp2->n)
+		{
+			tmp1 = tmp1->next;
+			tmp2 = tmp2->next;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	if (tmp1 == NULL && tmp2 == NULL)
+	{
+		return (1);
+	}
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a singly linked list
+ * is a palindrome
+ * @head: pointer to head of list
+ * Return: 0 if it is not a palindrome,
+ * 1 if it is a palndrome
  */
 int is_palindrome(listint_t **head)
 {
-	struct listint_s *node = *head;
-	long long int forward;
-	long long int reverce;
+	listint_t *slow, *fast, *prev_slow;
+	listint_t *scn_half, *middle;
+	int isp;
 
-	if (*head == NULL)
-		return (1);
+	slow = fast = prev_slow = *head;
+	middle = NULL;
+	isp = 1;
 
-	forward = 1;
-	reverce = 1;
-	while (node != NULL)
+	if (*head != NULL && (*head)->next != NULL)
 	{
-		forward = ((forward) + (node->n)) % sizeof(long long int) + 1;
-		reverce = ((reverce) + (node->n * -1)) % sizeof(long long int) + 1;
-		node = node->next;
+		while (fast != NULL && fast->next != NULL)
+		{
+			fast = fast->next->next;
+			prev_slow = slow;
+			slow = slow->next;
+		}
+
+		if (fast != NULL)
+		{
+			middle = slow;
+			slow = slow->next;
+		}
+
+		scn_half = slow;
+		prev_slow->next = NULL;
+		reverse(&scn_half);
+		isp = compare(*head, scn_half);
+
+		if (middle != NULL)
+		{
+			prev_slow->next = middle;
+			middle->next = scn_half;
+		}
+		else
+		{
+			prev_slow->next = scn_half;
+		}
 	}
-	if (forward != reverce)
-		return (0);
-	return (1);
+
+	return (isp);
 }
