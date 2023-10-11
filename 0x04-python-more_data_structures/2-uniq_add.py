ab75b11eaf4b5096b8f82f5052ebff35022ed2ba
Amine Meftah <amine.gamer222@gmail.com>
2023-10-11 18:27:09 +0100

    inique bug fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,9 +4,9 @@ def uniq_add(my_list=[]):
     sum_ = 0
     for x in my_list:
         try:
-            if dict_[x] == True:
+            if dict_[x] is True:
                 pass
-        except:
+        except (Exception):
             sum_ += x
             dict_[x] = True
     return (sum_)
