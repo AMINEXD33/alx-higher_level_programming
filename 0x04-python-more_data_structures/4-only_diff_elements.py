7881bff34e066fcc065032975e2382f87cff9c99
Amine Meftah <amine.gamer222@gmail.com>
2023-10-11 18:35:14 +0100

    only diff

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,4 +4,7 @@ def only_diff_elements(set_1, set_2):
     for x in set_1:
         if x not in set_2:
             list_.append(x)
+    for x in set_2:
+        if x not in set_1:
+            list_.append(x)
     return (list_)
