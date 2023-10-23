b550bd17e7cc10f320366cabce5f717019fccaf1
Amine Meftah <amine.gamer222@gmail.com>
2023-10-23 20:23:06 +0100

    safe print int

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if (value == None):
-            return False
         value += 1
         print("{:d}".format(value - 1))
         return True
