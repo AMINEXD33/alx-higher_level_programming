fbaf75e51aa64882ef0ebb39c234a81a4e0c08ff
Amine Meftah <amine.gamer222@gmail.com>
2023-10-23 20:18:11 +0100

    safe print int

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,9 @@
+def safe_print_integer(value):
+    try:
+        if (type(value) is int):
+            print("{:d}".format(value))
+            return True
+        else:
+            return False
+    except TypeError:
+        return False
