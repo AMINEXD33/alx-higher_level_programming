47f88562f50a0ea7ea6b08a803770829ea6b32c1
Amine Meftah <amine.gamer222@gmail.com>
2023-10-24 18:11:20 +0100

    describe a square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 class Square():
     def __init__(self, height, width):
+        """
+            Describe a square
+        param height: the height of the square
+        param width: the width of the square
+        """
         self.height = height
         self.width = width
