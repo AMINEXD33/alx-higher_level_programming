3fec24874e33d6394c1e0f86e9c859c999b92877
Amine Meftah <amine.gamer222@gmail.com>
2023-10-24 19:56:05 +0100

    describe a square6

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""SQUARE"""
+
+
+class Square():
+    """ititiate the square size"""
+    def __init__(self, size=0):
+        """Initialize the size attr
+        size: the size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+            return
+        if size < 0:
+            raise ValueError("size must be >= 0")
+            return
+        self.__size = size
+
+    @property
+    def size(self):
+        """get the value of the protected attr"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set a new value to the protected attr"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+            return
+        if value < 0:
+            raise ValueError("size must be >= 0")
+            return
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate the area of a square
+        return: the square area
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """
+        Print the square as '#'
+        if the size is 0 , this function
+        will print nothing
+        """
+        if self.__size == 0:
+            print()
+            return
+        else:
+            for x in range(self.__size):
+                for y in range(self.__size):
+                    print("#", end="")
+                print("\n", end="")
