9f1ccc4e5cf66411d859a54016a3edd721f4aa0b
Amine Meftah <amine.gamer222@gmail.com>
2023-10-30 21:19:46 +0100

    rectangle class2

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""THIS CLASS DOES NOTHING"""
+
+
+class Rectangle():
+    """RECTANGLE"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise TypeError("width must be >= 0")
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise TypeError("height must be >= 0")
