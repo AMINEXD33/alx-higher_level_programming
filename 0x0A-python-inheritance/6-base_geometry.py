98d349a7322b85966a707253dd0f394975ec38b8
Amine Meftah <amine.gamer222@gmail.com>
2023-11-06 15:45:34 +0100

    geometry class2

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Geometry class"""
+
+
+class BaseGeometry():
+    """Does nothing right now"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception "area() is not implemented"
