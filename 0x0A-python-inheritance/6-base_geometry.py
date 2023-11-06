29266bb5368d8e3afdc480e4071bdcf4d8da6b27
Amine Meftah <amine.gamer222@gmail.com>
2023-11-06 15:46:23 +0100

    geometry class2

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,7 +5,9 @@
 class BaseGeometry():
     """Does nothing right now"""
     def __init__(self):
+        """do nothing"""
         pass
 
     def area(self):
+        """raise exception"""
         raise Exception "area() is not implemented"
