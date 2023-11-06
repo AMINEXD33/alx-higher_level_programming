36817a9357fef45e18933ba3c25784dd53bc4fe4
Amine Meftah <amine.gamer222@gmail.com>
2023-11-06 16:09:11 +0100

    Geometry class 4

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -9,6 +9,6 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """construc"""
         BaseGeometry.integer_validator(self, 'width', width)
-        self.width = width
         BaseGeometry.integer_validator(self, 'height', height)
-        self.height = height
+        self.__width = width
+        self.__height = height
