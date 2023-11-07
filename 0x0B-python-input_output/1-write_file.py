98808aa0e24d0bc35d802faf3ff4f0c88e8c9a26
Amine Meftah <amine.gamer222@gmail.com>
2023-11-07 18:39:46 +0100

    write from a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Read files and stuff"""
+
+
+def write_file(filename="", text=""):
+    """Read a file UNC=utf-8
+       args:
+           filename : the name of the file
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        text = f.write(text)
