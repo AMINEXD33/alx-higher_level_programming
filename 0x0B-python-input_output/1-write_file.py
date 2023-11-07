ca8394d7515ba9c88d4dfc54a6de29c223cb2620
Amine Meftah <amine.gamer222@gmail.com>
2023-11-07 18:43:41 +0100

    write from a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,4 +8,4 @@ def write_file(filename="", text=""):
            filename : the name of the file
     """
     with open(filename, "w", encoding="utf-8") as f:
-        return (f.writelines(text))
+        return (f.write(text))
