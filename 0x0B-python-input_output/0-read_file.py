56ea9cb9948e1d3669c0a9bb3727c1905aa659be
Amine Meftah <amine.gamer222@gmail.com>
2023-11-07 18:28:01 +0100

    read from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 """Read a file UNC=utf-8"""
+
+
 def read_file(filename=""):
-	with open("filename", "r", encoding="utf-8") as f:
-		text = f.read()
-		print(text)
-	
+    with open("filename", "r", encoding="utf-8") as f:
+        text = f.read()
+        print(text)
