917e78c389b95ea54281939b30b72343a9367d02
Amine Meftah <amine.gamer222@gmail.com>
2023-11-07 20:17:00 +0100

    file->obj obj->file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,10 +2,11 @@
 """Add all arguments to a Py list and save them"""
 import sys
 
+
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
-    __import__('6-load_from_json_file').load_from_json_file
+        __import__('6-load_from_json_file').load_from_json_file
 
     try:
         items = load_from_json_file("add_item.json")
