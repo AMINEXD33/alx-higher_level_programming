e2ef230c40325ee4a3a427fa1637839ac63f3316
Amine Meftah <amine.gamer222@gmail.com>
2023-11-07 19:13:08 +0100

    obj->json->save in file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""using json"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """obj-> json -> write to file"""
+    json_txt = json.dumps(my_obj)
+    with open(filename, 'w', unicode="utf-8") as f:
+        return f.write(json_txt)
