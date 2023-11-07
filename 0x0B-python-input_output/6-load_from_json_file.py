e22c2694065a6963b77be90085f7fdca63fae478
Amine Meftah <amine.gamer222@gmail.com>
2023-11-07 19:28:33 +0100

    file->obj

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -6,4 +6,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """file -> obj"""
     with open(filename, 'r', encoding="utf-8") as f:
-        return json.loaod(f)
+        return json.load(f)
