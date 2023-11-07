b2d7995333100682091da93f59817207ff3a738f
Amine Meftah <amine.gamer222@gmail.com>
2023-11-07 18:29:53 +0100

    read from a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 
 def read_file(filename=""):
-    with open("filename", "r", encoding="utf-8") as f:
+    with open(filename, "r", encoding="utf-8") as f:
         text = f.read()
         print(text)
