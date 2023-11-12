4ef90ad3d1fe7f0369194fa9297e9db3307c4666
Amine Meftah <amine.gamer222@gmail.com>
2023-11-12 23:04:21 +0100

    Almost a cercle project , needs some work on the tests

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-"""THE BASE MODULE"""
+"""THE BASE MODULE
+this module is the base of other modules
+"""
+import json
 
 
 class Base():
-    """the base class definition"""
+    """the base class definition
+        attributes:
+        id: the id of the instances
+    """
 
     __nb_objects = 0
 
@@ -14,3 +20,80 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return a js object from a list of dicts
+            Args:
+                list_dictionaries: the list of dicts
+        """
+        if (list_dictionaries is None):
+            return ([])
+        elif (len(list_dictionaries) == 0):
+            return ([])
+        js = json.dumps(list_dictionaries)
+        return (js)
+
+    def save_to_file(cls, list_objs):
+        """save the json list to a file
+           args:
+                cls:
+                list_objs:
+        """
+        js_ = Base.to_json_string(list_objs)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON serialization of a list of objects to a file.
+
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w", encoding='utf-8') as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """from a text json to a dictionary
+           Args:
+               json_string: the txt json represantation
+        """
+        if (json_string is None):
+            return ([])
+        elif (len(json_string) == 0 or json_string == ""):
+            return ([])
+        dict_ = json.loads(json_string)
+        return (dict_)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return a class instantied from a dictionary
+        of attributes
+        Args:
+            **dictionary: a dict of attributes
+        """
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Loads an object from a file
+        and return a list of instances
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""Square module that represents a Square
+with a size
+"""
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """constructor"""
+        super().__init__(size,
+                         size,
+                         x,
+                         y,
+                         id)
+
+    @property
+    def size(self):
+        """Get/Set the size of the square"""
+        return (self.height)
+
+    @size.setter
+    def size(self, val: int):
+        self.width = val
+        self.height = val
+
+    def __str__(self):
+        """return a str represantation of this instance"""
+        return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
+
+    def update(self, *args, **kwargs):
+        """ update the object's attrs
+            use args if it exists,
+            else use kwargs
+            args:
+                args: a list of arguments
+                kwargs: a dict of args
+        """
+        attr_ = ["id",
+                 "size",
+                 "x",
+                 "y"]
+        if (len(args) != 0):
+            for x in range(len(args)):
+                if (x > len(attr_)-1):
+                    break
+                setattr(self, attr_[x], args[x])
+        else:
+            for key, val in kwargs.items():
+                if (key == attr_[1]):
+                    self.height = val
+                    self.width = val
+                elif (key in attr_):
+                    setattr(self, key, val)
+
+    def to_dictionary(self):
+        """return a dictionary that has
+            all attrs
+        """
+        return ({
+            'id': self.id,
+            'size': self.size,
+            'x': self.x,
+            'y': self.y
+        })
