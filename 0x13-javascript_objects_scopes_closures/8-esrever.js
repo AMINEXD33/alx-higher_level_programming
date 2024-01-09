7a5dd0c19f63dab428605a0d4a59b2c24615f152
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-09 19:23:23 +0100

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,7 +4,7 @@ exports.esrever = function (list) {
   // they cou've made it harder by
   // asking to sort in place
   const EmptyList = [];
-  for (let x = list.length; x > 0; x--) {
+  for (let x = list.length; x >= 0; x--) {
     EmptyList.push(list[x]);
   }
   list = EmptyList;
