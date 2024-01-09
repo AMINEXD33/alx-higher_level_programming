77785fa8d91a83cbe0554c59aea159d4bed450c5
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-09 19:50:00 +0100

    task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,13 +2,5 @@
 
 const list = require('./100-data.js').list;
 
-let mapVar = new Map();
-let result = [];
-for (let x = 0; x < list.length; x++)
-{
-    mapVar.set(x,(list[x] * x));
-}
-for (let x = 0; x < list.length; x++)
-{
-    result.push(mapVar.get(x));
-}
+console.log(list);
+console.log(list.map((item, index)=> item * idex));
