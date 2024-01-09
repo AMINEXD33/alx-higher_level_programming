c3b07c43fbc6a687cc8488057c57d057c9e8a70d
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-09 19:17:51 +0100

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,18 +1,15 @@
 #!/usr/bin/node
 
-exports.nbOccurences = fucntion(list, target)
-{
-    if (list === undefined || list.length === 0){
-        return (0);
-    }
+exports.nbOccurences = function (list, target) {
+  if (list === undefined || list.length === 0) {
+    return (0);
+  }
 
-    let occ = 0;
-    for (let x = 0; x < list.length; x ++)
-    {
-        if (list[x] === target)
-        {
-            occ++;
-        }
+  let occ = 0;
+  for (let x = 0; x < list.length; x++) {
+    if (list[x] === target) {
+      occ++;
     }
-    return (occ);
-}
+  }
+  return (occ);
+};
