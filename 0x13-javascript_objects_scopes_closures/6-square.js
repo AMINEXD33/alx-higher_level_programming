7fec5146c32dde73a9e1e78872f60e1972a3fbc3
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-09 19:07:37 +0100

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const SquareOne = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends SquareOne {
   charPrint (char) {
     if (char === undefined) {
       super.print();
@@ -20,4 +16,5 @@ class Square extends Rectangle {
     }
   }
 }
+
 module.exports = Square;
