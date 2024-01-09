a3f3d45b784c121515b444a1ed4a2f7db1649007
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-09 20:20:05 +0100

    task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,7 @@
 #!/usr/bin/node
 
-// const dict = require('./100-data.js').dict;
-let dict = {
-  89: 1,
-  90: 2,
-  91: 1,
-  92: 3,
-  93: 1,
-  94: 2
-};
-dict = Object.entries(dict);
+const dict = require('./100-data.js').dict;
 const newMap = new Map();
-
 for (const vals of dict.values()) {
   const val = vals[0];
   const key = vals[1].toString();
