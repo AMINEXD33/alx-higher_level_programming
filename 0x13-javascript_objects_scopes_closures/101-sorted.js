bc7160c5948299546d46e4d1f4fbe18ac0877501
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-09 20:20:27 +0100

    task 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,7 +6,6 @@ for (const vals of dict.values()) {
   const val = vals[0];
   const key = vals[1].toString();
   if (newMap.has(key)) {
-    console.log(key, 'with', val, 'already in ');
     const tmparr = newMap.get(key);
     tmparr.push(val);
     newMap.set(key, tmparr);
