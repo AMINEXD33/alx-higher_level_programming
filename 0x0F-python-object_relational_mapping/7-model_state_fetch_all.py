e869e5ae274efeb40486e2aa03e1924c76cf78a1
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-12 21:46:41 +0100

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,24 +1,16 @@
 #!/usr/bin/python3
-"""create the State class"""
+"""LIST ALL STATES FROM DB hbnt_0e_0_usa"""
 
 from sys import argv
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+from model_state import Base, State
 from sqlalchemy import Column, Integer, String
-Base = declarative_base()
-
-
-class State(Base):
-    """Class state"""
-    __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True,
-                primary_key=True, nullable=False, unique=True)
-    name = Column(String(128), nullable=False)
-
 
 if __name__ == "__main__":
     import sys
+
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
