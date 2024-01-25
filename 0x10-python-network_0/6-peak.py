30d7befc5a2d24d4a0dab3085063f1736944b8e4
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-26 00:41:24 +0100

    task 7

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
 """find_peak
-    @pram list_of_integersof_integers: a list of integers
+    @pram list_of_integers: a list of integers
 """
+
+
 def find_peak(list_of_integers):
 
-    list_of_integers
+    ls = list_of_integers
+    ls
     # if there is no list of integers return None
-    if list_of_integers == []:
+    if ls == []:
         return None
-    length = len(list_of_integers)
+    length = len(ls)
 
     start, end = 0, length - 1
     while start < end:
         mid = start + (end - start) // 2
-        if list_of_integers[mid] > list_of_integers[mid - 1] and list_of_integers[mid] > list_of_integers[mid + 1]:
-            return list_of_integers[mid]
-        if list_of_integers[mid - 1] > list_of_integers[mid + 1]:
+        if ls[mid] > ls[mid - 1] and ls[mid] > ls[mid + 1]:
+            return ls[mid]
+        if ls[mid - 1] > ls[mid + 1]:
             end = mid
         else:
             start = mid + 1
