cafb79cf65b524787d88fc9ff03231b9859570d2
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-25 21:45:20 +0100

    task 2

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # get the body of a 200 status request only
-res=$(curl -sI "$1");if [ "$( echo "$res" | grep -Po '(?<=HTTP/\d.\d) \d{3}')" -eq "200" ];then "$(curl -sX GET "$1")"; fi
+curl -Ls $1
 
