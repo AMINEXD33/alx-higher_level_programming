e9540589e26afa138fcf56bfaf4bc63f71b1600f
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-26 20:38:19 +0100

    fixed 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """
-Create a script that accepts a URL as input, 
-sends a request to the specified URL, 
-and presents the decoded utf-8 body of the response. 
-Ensure proper handling of urllib.error.HTTPError exceptions and, 
-in case of an exception, 
+Create a script that accepts a URL as input,
+sends a request to the specified URL,
+and presents the decoded utf-8 body of the response.
+Ensure proper handling of urllib.error.HTTPError exceptions and,
+in case of an exception,
 print: "Error code:" followed by the corresponding HTTP status code.
 """
 import urllib.request
