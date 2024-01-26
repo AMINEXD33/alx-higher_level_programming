a0066215978c8ea7647c2a679b59a957a9179b29
AMINEXD33 <amine.gamer222@gmail.com>
2024-01-26 20:11:07 +0100

    task2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+"""
+a python script that gets the value of a header variable [ X-Request-Id ]
+from a response
+"""
 import urllib.request
 import sys
 
 
-with urllib.request.urlopen(sys.argv[1]) as res:
-    html = res.info()
-    print(html.get('X-Request-Id'))
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as res:
+        html = res.info()
+        print(html.get('X-Request-Id'))
