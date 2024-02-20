8aa26ea5b12176e452a383fdabefdc38fbde1d92
AMINEXD33 <amine.gamer222@gmail.com>
2024-02-20 11:28:35 +0100

    logic change

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,12 +4,11 @@
 const fs = require('fs');
 const { argv } = require('node:process');
 
-function writeFfile (fileName, fileContent) {
-  fs.writeFile(fileName, fileContent, 'utf-8', (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
-}
+const fileName = argv[2];
+const fileContent = argv[3];
 
-writeFfile(argv[2], argv[3]);
+fs.writeFile(fileName, fileContent, 'utf-8', (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
